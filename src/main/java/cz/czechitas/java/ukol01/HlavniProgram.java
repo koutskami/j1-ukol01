e3dcef7d30756a770c2197599a114c47b0d7d520
koutskami <koutskami@gmail.com>
2024-02-03 19:59:17 +0100

    treti cast domaciho ukolu - vykresleni rady domku

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -4,7 +4,6 @@ import cz.czechitas.java.ukol01.engine.Turtle;
 
 public class HlavniProgram {
   private Turtle zofka;
-
   public static void main(String[] args) {
     new HlavniProgram().start();
   }
@@ -13,6 +12,70 @@ public class HlavniProgram {
     zofka = new Turtle();
 
     //TODO implementace domácího úkolu
+
+    //Cast 3 - rada domku:
+    //presun zofky na levou stranu plochy
+    odchodVlevo(450);
+
+    //cyklus pro nakresleni rady domku
+    for (int i = 0; i < 5; i++) {
+      nakresliDomek(100, 60);
+      popojdiDoprava(90,60);
+    }
+
+    //zofka nakresli posledni dva domky o radu nize
+    zofka.turnRight(90); //Zofka kouka dolu
+    zofka.move(200); //Zofka se posunula dolu
+    zofka.turnLeft(180); //Zofka kouka nahoru
+    odchodVlevo(100+60); //Zofka se zarovna s hornim domkem - jde o soucet strCtverce a pixels z popojdiDoprava
+    nakresliDomek(100,60); //Zofka skonci a kouka dolu
+    zofka.turnLeft(180); //Zofka kouka nahoru
+    odchodVlevo(5*(100+60)-30); //presun pod prvni domek - odpovida priblizne strCverce + pixels z popojdiDoprava * pocet iteraci
+    nakresliDomek(100,60); //rada domku je hotova
+    //TODO zjistit jak vyrobit promennou z parametru metod
+  }
+  public void nakresliCtverec(int strCtverce) {
+    zofka.penDown();
+    for (int i = 0; i < 4; i++) {
+      int pravyUhel = 90;
+      zofka.move(strCtverce);
+      zofka.turnRight(pravyUhel);
+    }
+  }
+  /** *Metoda nakreslí dvě stěny trojúhelníku bez báze s úhlem 60°.
+   * Parametr je délka strany. Pro čtverec o 100 px je vhodná délka strany střechy 60 px* **/
+  public void nakresliStrechu(int strStrechy) {
+    zofka.turnLeft(30);
+    for (int i = 0; i < 2; i++) {
+      int uhelTrojuhelniku = 60;
+      zofka.move(strStrechy);
+      zofka.turnRight(uhelTrojuhelniku);
+      //TODO druhá strana trochu přesahuje, šlo by vylepšit
+    }
   }
 
+  //Seznam metod:
+  /** * Aby Žofka odešla vlevo, tak musí být otočená nahoru.
+   * Parametr je vzdálenost, kterou Žofka ujde.
+   * Po použití metody je Žofka nachystaná na metodu nakresliDomek
+   * Z půlky pole je to cca 450 pixelů. * **/
+  public void odchodVlevo(int pixels) {
+    zofka.penUp();
+    zofka.turnLeft(90);
+    zofka.move(pixels);
+    zofka.turnLeft(180);
+  }
+  /** * Metoda nakreslí čtverec se střechou.
+   * Žofka zůstane koukat dolů pod 30° úhlem od horní stěny čtverce * **/
+  public void nakresliDomek(int strCtverce, int strStrechy) {
+    nakresliCtverec(strCtverce);
+    nakresliStrechu(strStrechy);
+  }
+  /** * Metoda vytvořená pro posun doprava, když Žofka kouká dolů.
+   * Pro posun po dokreslení domečku zadej úhel 90° a 60 px. Žofka se tím srovná s horní stěnou čtverce* **/
+  public void popojdiDoprava(int byAngle, int pixels) {
+    zofka.penUp();
+    zofka.turnLeft(byAngle);
+    zofka.move(pixels);
+  }
 }
